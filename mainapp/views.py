6010817b3baaab9384feb92cbbe1b930164d8737
YOUR NAME <halichm96@gmail.com>
2015-03-05 19:41:45 +0200

    v1.2.6

--- mainapp/views.py
+++ mainapp/views.py
@@ -37,29 +37,7 @@ def TempSave(request, objct=objct):
             cd = form.cleaned_data
             act = Activity(time_stamp=dt.now(), type='Getting on temporary storage', actor=request.user)
             act.save()
-            obj.name_title=cd['name']
-            obj.collection=cd['collection']
-            obj.is_fragment=cd['is_fragment']
-            obj.amount=cd['amount']
-            obj.material=cd['material']
-            #obj.size_type=cd['size_type']
-            #obj.size_number=ast.literal_eval(cd['size'][0:-1].split(':')[0])
-            #obj.size_measurement_unit=ast.literal_eval(cd['size'][0:-1].split(':')[1])
-            obj.size_number=cd['size']
-            obj.author=cd['author']
-            obj.description=cd['description']
-            #obj.condition_descr=cd['condition_descr']
-            obj.condition=cd['condition']
-            obj.price=cd['price']
-            obj.note=cd['note']
-            obj.technique=cd['technique']
-            obj.place=cd['storage']
-            obj.transferred_from=cd['transferred_from']
-            obj.transferred_to=cd['transferred_to']
-            obj.term_back=cd['term_back']
-            obj.aim_of_receiving_gen=cd['aim_of_receiving']
-            obj.way_of_found=cd['way_of_found']
-            obj.identifier=cd['code']
+            obj = Object()
             obj.save()
             for (k, v) in cd.items():
                 attr_assign=AttributeAssignment(attr_name=k, attr_value=v, event_initiator=act, aim=obj)
