3429ced43d42d522b45ee244b09e5261167c4e71
YOUR NAME <halichm96@gmail.com>
2015-03-05 19:43:49 +0200

    v1.2.6

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,31 +8,12 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-objct = []
 
 @csrf_protect
 @login_required
-#def InitialTempSave(request, objct=objct):
- #  if request.method == 'POST':
-#	form = InitialTempSaveForm(request.POST)
- #       if form.is_valid():
-  #         objct.append(form.cleaned_data['obj'])
-   #     return HttpResponseRedirect('init/')
-    #else: 
-     #   form = InitialTempSaveForm()
-      #  return render(request, 'form.html', {'form': form})
-
-@csrf_protect
-@login_required         
-#def TempSave(request, objct=objct):
-    #try: 
-	#obj = objct[0]
-    #except IndexError:
-     #   obj = Object()
-    #initial = {'name': obj.name_title, 'collection': obj.collection, 'author': obj.author, 'amount': obj.amount, 'is_fragment': obj.is_fragment,'size': obj.size_number, 'description': obj.description, 'condition': obj.condition, 'price': obj.price, 'material': obj.material, 'note': obj.note, 'technique': obj.technique, 'storage': obj.place, 'transferred_from': obj.transferred_from,  'transferred_to': obj.transferred_to, 'term_back': obj.term_back, 'aim_of_receiving': obj.aim_of_receiving_gen, 'way_of_found': obj.way_of_found, 'code': obj.identifier} 
-def TempSave(request, objct=objct):
+def TempSave(request):
     if request.method == 'POST':
-        form = TempSaveForm(request.POST, initial=initial)
+        form = TempSaveForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             act = Activity(time_stamp=dt.now(), type='Getting on temporary storage', actor=request.user)
